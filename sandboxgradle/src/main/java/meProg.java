f9c977eb95875fdafb19d06e7368491966a95505
Yonathan <dikijgon4ik@gmail.com>
2017-06-15 17:22:06 +0300

    12

--- sandboxgradle/src/main/java/meProg.java
+++ sandboxgradle/src/main/java/meProg.java
@@ -6,5 +6,23 @@ public static void main(String[] args){
 
 public static void hello(String somebody){
   System.out.println("Hello, my wonderful " + somebody);
+
+  double a = 5;
+  double b = 2;
+
+  Square s = new Square(5);
+  System.out.println("Площадь квадрата = " + area(s));
+
+  Rectangle r = new Rectangle(2,4);
+  System.out.println("Площадь прямоугольника = " + area(r));
+
+}
+
+public static double area(Square s){
+  return s.a;
+}
+
+public static double area(Rectangle r){
+  return r.a*r.b;
 }
 }
